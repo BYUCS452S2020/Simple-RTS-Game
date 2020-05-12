bc88117ea8733e04e41f51cf7d60ed5a8397fcef
michaelblack117 <michaelblack117@gmail.com>
2020-05-12 13:52:57 -0600

    converting script to react component for rendering map

--- SimpleRTSGameUI/src/Components/GameMap/MapRenderer.js
+++ SimpleRTSGameUI/src/Components/GameMap/MapRenderer.js
@@ -1,22 +1,5 @@
-
 import React from 'react';
-
-var map = require("../../Assets/Maps/Level1.json");
-function getTilesFromMap(m) {
-  let tilesets = m.tilesets
-  let id = 1;
-  let tiles = {}
-  for (let i = 0; i < tilesets.length; i++) {
-    let t = tilesets[i].tiles;
-    for (let j = 0; j < t.length; j++) {
-      tiles[id] = new Image();
-      tiles[id].src = t[j].image.replace("..", "../../Assets");
-      id++;
-    }
-  }
-  return tiles;
-}
-var tiles = getTilesFromMap(map);
+import './MapRenderer.css';
 
 class MapRenderer extends React.Component {
   constructor(props) {
@@ -26,61 +9,91 @@ class MapRenderer extends React.Component {
     }
   }
   componentDidMount() {
-    this.renderLayers(map.layers);
+    fetch('Assets/Maps/Level1.json').then(resp => {
+      resp.json().then(json => {
+        this.map = json;
+        this.tiles = this.getTilesFromMap(this.map);
+        this.renderLayers(this.map.layers);
+      })
+    });
   }
-  componentDidUpdate(prevProps) {
 
-  }
-  renderLayer = (layer) => {
-    if (layer.type !== "tilelayer" || !layer.opacity) { return; }
-    let c = document.getElementById("canvas").getContext("2d");
-    console.log(c);
-    if (layer.data[0] !== 0) {
-      // console.log(layer.data[0]);
-      console.log(tiles[58]);
-      c.drawImage(tiles[58], 0, 0);
+  getTilesFromMap = (m) => {
+    let tilesets = m.tilesets
+    let id = 1;
+    let tiles = {}
+    for (let i = 0; i < tilesets.length; i++) {
+      let t = tilesets[i].tiles;
+      for (let j = 0; j < t.length; j++) {
+        tiles[id] = t[j].image.replace("..", "Assets");
+        id++;
+      }
     }
+    return tiles;
+  }
 
-    // c.drawImage(layer.data[0], 0, 0);
-    // let s = c.canvas.cloneNode();
-    // let size = this.map.tilewidth;
-    // s = s.getContext("2d");
+  renderLayer = (layer) => {
+    let canvas = document.getElementById(layer.name);
+    let ctx = canvas.getContext("2d");
+    let x, y = 0;
 
-    // if (scene.layers.length < scene.data.layers.length) {
-    //   layer.data.forEach(function(tile_idx, i) {
-    //     if (!tile_idx) { return; }
-    //     var img_x, img_y, s_x, s_y,
-    //           tile = scene.data.tilesets[0];
-    //     tile_idx--;
-    //     img_x = (tile_idx % (tile.imagewidth / size)) * size;
-    //     img_y = ~~(tile_idx / (tile.imagewidth / size)) * size;
-    //     s_x = (i % layer.width) * size;
-    //     s_y = ~~(i / layer.width) * size;
-    //     s.drawImage(scene.tileset, img_x, img_y, size, size,
-    //                         s_x, s_y, size, size);
-    //   });
-    //   scene.layers.push(s.canvas.toDataURL());
-    //   c.drawImage(s.canvas, 0, 0);
-    // }
-    // else {
-    //   scene.layers.forEach(function(src) {
-    //     var i = $("<img />", { src: src })[0];
-    //     c.drawImage(i, 0, 0);
-    //   });
-    // }
+    let i = 0;
+    if (layer.data[i]) {
+      let img = new Image();
+      img.src = this.tiles[layer.data[i]]
+      img.onload = function() {
+        console.log("drawing", layer.data[i], img.src);
+        ctx.drawImage(img, x, y);
+      }
+    }
+    x = (x + this.map.tilewidth) % (this.map.width * this.map.tilewidth);
+    y = this.map.tileheight * (Math.floor(i / this.map.width));
+    i = 1;
+    if (layer.data[i]) {
+      let img = new Image();
+      img.src = this.tiles[layer.data[i]]
+      img.onload = function() {
+        ctx.drawImage(img, x, y);
+      }
+    }
+    x = (x + this.map.tilewidth) % (this.map.width * this.map.tilewidth);
+    y = this.map.tileheight * (Math.floor(i / this.map.width));
+    i = 2;
+    if (layer.data[i]) {
+      let img = new Image();
+      img.src = this.tiles[layer.data[i]]
+      img.onload = function() {
+        ctx.drawImage(img, x, y);
+      }
+    }
+    // layer.data.forEach((id, i) => {
+    //   if (id) {
+    //     let img = new Image();
+    //     img.src = this.tiles[layer.data[id]];
+    //     img.onload = function() {
+    //       console.log("drawing", id, img);
+    //       ctx.drawImage(img, x, y);
+    //     }
+    //   }
+    //   x = (x + this.map.tilewidth) % (this.map.width * this.map.tilewidth);
+    //   y = this.map.tileheight * (Math.floor(i / this.map.width));
+    // });
   }
 
   renderLayers = (layers) => {
-    layers = Array.isArray(layers) ? layers : []
     layers.forEach(this.renderLayer);
   }
 
   render() {
-    if (!this.props.map) { return <div></div> }
+    if (!this.props.map) { return <div>Map not provided</div> }
     return (
-      <canvas id="canvas">
-      </canvas>
-    )
+      <div style={{ position: "relative" }}>
+        <canvas id="Floor" style={{ zIndex: 0 }}></canvas>
+        <canvas id="Environment" style={{ zIndex: 1 }}></canvas>
+        <canvas id="Units" style={{ zIndex: 2 }}></canvas>
+        <canvas id="Structures" style={{ zIndex: 3 }}></canvas>
+      </div>
+    );
   }
 }
 
